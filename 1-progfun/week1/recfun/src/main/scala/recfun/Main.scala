267a762b58302ceb2069abfddc75c87e9b182501
Sam Bott <sam.bott@gmail.com>
2016-07-03 13:20:30 +0100

    week1 tests pass

--- 1-progfun/week1/recfun/src/main/scala/recfun/Main.scala
+++ 1-progfun/week1/recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -11,17 +13,50 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    require(c >= 0 && c <= r + 1)
+    c match {
+      case 0 => 1
+      case `r` => 1
+      case n => pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def inner(remainingChars: List[Char], open: Int): Boolean = {
+      remainingChars match {
+        case Nil => open == 0
+        case '(' :: t => inner(t, open + 1)
+        case ')' :: t => if (open == 0) false else inner(t, open - 1)
+        case _ :: t => inner(t, open)
+      }
+    }
+    inner(chars, 0)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def inner(remaining: Int, coinsLeft: List[Int]): Int = {
+      coinsLeft.map{coin =>
+        remaining - coin match {
+          case n if n < 0 => 0
+          case 0 => 1
+          case n => inner(n, coinsLeft.filter(_ <= coin))
+        }
+      }.sum
+    }
+    money match {
+      case n if n < 0 => 0
+      case 0 => 1
+      case n => inner(n, coins)
+    }
+  }
+}
